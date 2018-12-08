f1992bc9986965766c23a0c42b25eed3c8b08198
SmbatGrigoryan <smbatgrigoryan@gmail.com>
2018-12-08 12:07:43 +0400

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -6,11 +6,11 @@ const config = {
     mongoURI: process.env.MONGO_URI
   },
   defaultDev: {
-    SECRET_KEY: 'somemysterioussecretkey',
+    SECRET_KEY: '*******************************',
     DATABASE: 'mongodb://localhost:27017/library', // todo remove later
-    mongoURI: 'mongodb://smbat-libraryapp-prod:SGsg_123@ds227674.mlab.com:27674/libraryapp'
+    mongoURI: 'mongodb://*******************:***********@ds227674.mlab.com:27674/libraryapp'
   }
-  //mongodb://<dbuser>:<dbpassword>@ds227674.mlab.com:27674/libraryapp
+  
 };
 
 const configKeys = (env) => {
